a7f0b190775196a788a3335ed70c840e86ff8a88
David-Jonathan <silas.jonathan@gmail.com>
2017-07-19 20:36:24 +1000

    Delete LivingSpaceTest.java

--- src/main/java/com/cc/game/util/InvalidInputException.java
+++ src/main/java/com/cc/game/util/InvalidInputException.java
@@ -8,6 +8,11 @@ package com.cc.game.util;
 
 public class InvalidInputException extends Exception {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
 	public InvalidInputException(String errorMessage) {
 		super(errorMessage);
 	}
